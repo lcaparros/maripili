60fe2df362552191f7f52b3af26b0aec0feddb44
lcaparros <luiscaparrosruiz@gmail.com>
2021-07-07 12:50:18 +0200

    Changed parameters order for telegram bash send message function

--- shell/telegram_functions.sh
+++ shell/telegram_functions.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 TOKEN="${BOT_TELEGRAM_TOKEN}"
-CHAT_ID="$2"
+CHAT_ID="$1"
 
 TELEGRAM_API_URL="https://api.telegram.org/bot${TOKEN}"
 
 sendTelegramNotification() {
     echo "New Telegram Notification"
     echo "Chat ID: ${CHAT_ID}"
-    echo "Message: $1"
+    echo "Message: $2"
 
     curl \
         --request GET \
         --data "chat_id=${CHAT_ID}" \
         --data "parse_mode=Markdown" \
-        --data-urlencode "text=$1" \
+        --data-urlencode "text=$2" \
         "${TELEGRAM_API_URL}/sendMessage"
 }
 
