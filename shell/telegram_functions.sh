114258f599592ca631d3d53dba3ea02a335d7b0d
lcaparros <luiscaparrosruiz@gmail.com>
2021-07-07 12:30:44 +0200

    Added new send message function from bash/CLI

---
+++ shell/telegram_functions.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+TOKEN="${BOT_TELEGRAM_TOKEN}"
+CHAT_ID="$2"
+
+TELEGRAM_API_URL="https://api.telegram.org/bot${TOKEN}"
+
+sendTelegramNotification() {
+    echo "New Telegram Notification"
+    echo "Chat ID: ${CHAT_ID}"
+    echo "Message: $1"
+
+    curl \
+        --request GET \
+        --data "chat_id=${CHAT_ID}" \
+        --data "parse_mode=Markdown" \
+        --data-urlencode "text=$1" \
+        "${TELEGRAM_API_URL}/sendMessage"
+}
+
