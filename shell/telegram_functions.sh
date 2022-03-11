44b15dc15a6a7f55b8316abcdf322e87cc109572
lcaparros <luiscaparrosruiz@gmail.com>
2022-03-11 09:22:00 +0100

    Fixed an error in telegram_functions shell script

--- shell/telegram_functions.sh
+++ shell/telegram_functions.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 
 TOKEN="${BOT_TELEGRAM_TOKEN}"
-CHAT_ID="$1"
 
 TELEGRAM_API_URL="https://api.telegram.org/bot${TOKEN}"
 
 sendTelegramNotification() {
+    CHAT_ID="$1"
+    MESSAGE="$2"
+
     echo "New Telegram Notification"
     echo "Chat ID: ${CHAT_ID}"
-    echo "Message: $2"
+    echo "Message: ${MESSAGE}"
 
     curl \
         --request GET \
         --data "chat_id=${CHAT_ID}" \
         --data "parse_mode=Markdown" \
-        --data-urlencode "text=$2" \
+        --data-urlencode "text=${MESSAGE}" \
         "${TELEGRAM_API_URL}/sendMessage"
 }
 
