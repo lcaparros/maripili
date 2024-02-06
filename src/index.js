6e0d615a279bd85f100963448bb68eebef6c2d2c
lcaparros <luiscaparrosruiz@gmail.com>
2024-02-06 13:29:41 +0100

    Adding logs

--- src/index.js
+++ src/index.js
@@ -30,13 +30,16 @@ schedule.scheduleJob('0 8 * * *', async () => {
     limit: 1,
     type
   })
+  logInfo(`Sending goat gif to ${chatIdDesertores}`)
   bot.sendAnimation(chatIdDesertores, gifs[0].images.original.url)
   await sleepMs(1000)
+  logInfo(`Sending good morning message to ${chatIdDesertores}`)
   bot.sendMessage(chatIdDesertores, getElementFromArray(frasesBuenosDias))
 })
 logInfo('Good morning desertores scheduled')
 
 schedule.scheduleJob('59 23 * * *', () => {
+  logInfo(`Sending good night message to ${chatIdDesertores}`)
   bot.sendMessage(chatIdDesertores, 'A ustedes!!')
 })
 logInfo('Good night desertores scheduled')
@@ -52,11 +55,13 @@ bot.onText(/^\/start/, function (msg) {
   const chatId = msg.chat.id
   const nameUser = msg.from.first_name
 
+  logInfo(`Sending response to start comand to ${chatId}`)
   bot.sendMessage(chatId, `Que dise ${nameUser}?? Así no funsiona esto kabesa`)
 })
 
 bot.onText(/maripili|mari pili/i, function (msg) {
   const chatId = msg.chat.id
+  logInfo(`Mari pili has been called in ${chatId}`)
   bot.sendMessage(chatId, getElementFromArray(frasesMariPili), {
     reply_to_message_id: msg.message_id
   })
@@ -64,6 +69,7 @@ bot.onText(/maripili|mari pili/i, function (msg) {
 
 bot.onText(new RegExp(insultos, 'i'), function (msg) {
   const chatId = msg.chat.id
+  logInfo(`Insult said in ${chatId}`)
   bot.sendMessage(chatId, getElementFromArray(respuestasInsultos), {
     reply_to_message_id: msg.message_id
   })
@@ -72,18 +78,22 @@ bot.onText(new RegExp(insultos, 'i'), function (msg) {
 bot.onText(/chiquito/i, function (msg) {
   const chatId = msg.chat.id
   if (randomFromPercentage(30)) {
+    logInfo(`Sending Chiquito voice to ${chatId}`)
     bot.sendVoice(chatId, getElementFromArray(voicesChiquito))
   } else {
+    logInfo(`Sending Chiquito message to ${chatId}`)
     bot.sendMessage(chatId, getElementFromArray(frasesChiquito))
   }
 })
 
 bot.onText(/lopera/i, function (msg) {
   const chatId = msg.chat.id
+  logInfo(`Sending Lopera message to ${chatId}`)
   bot.sendMessage(chatId, getElementFromArray(frasesLopera))
 })
 
 bot.onText(/homer/i, function (msg) {
   const chatId = msg.chat.id
+  logInfo(`Sending Homer voice to ${chatId}`)
   bot.sendVoice(chatId, getElementFromArray(voicesHomer))
 })
